14edc961fc7fd0158cec831ab047b17093deb0de
apham <apham@work>
2024-11-24 01:20:35 +0100

    shifting

--- config.def.h
+++ config.def.h
@@ -1196,8 +1196,10 @@ static const Key keys[] = {
 	#if FOCUSADJACENTTAG_PATCH
 	{ MODKEY|ControlMask,           XK_Left,       viewtoleft,             {0} }, // note keybinding conflict with focusdir
 	{ MODKEY|ControlMask,           XK_Right,      viewtoright,            {0} }, // note keybinding conflict with focusdir
-	{ MODKEY|ShiftMask,             XK_Left,       tagtoleft,              {0} }, // note keybinding conflict with shifttag
-	{ MODKEY|ShiftMask,             XK_Right,      tagtoright,             {0} }, // note keybinding conflict with shifttag
+	// { MODKEY|ShiftMask,             XK_Left,       tagtoleft,              {0} }, // note keybinding conflict with shifttag
+	// { MODKEY|ShiftMask,             XK_Right,      tagtoright,             {0} }, // note keybinding conflict with shifttag
+	{ MODKEY|ShiftMask,             XK_Left,       tagandviewtoleft,              {0} }, // note keybinding conflict with shifttag
+	{ MODKEY|ShiftMask,             XK_Right,      tagandviewtoright,             {0} }, // note keybinding conflict with shifttag
 	// { MODKEY|ControlMask,           XK_Left,       tagandviewtoleft,       {0} }, // note keybinding conflict with placedir
 	// { MODKEY|ControlMask,           XK_Right,      tagandviewtoright,      {0} }, // note keybinding conflict with placedir
 	#endif // FOCUSADJACENTTAG_PATCH
